d794c64512bbc3fd72474c4b69017024396a8de4
Kashyap Nasit <kashyapnasit@gmail.com>
2018-08-31 16:10:08 +0530

    add Timebar and volumeSlider.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,21 @@
 package sample;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
 import javax.naming.Binding;
 import java.io.File;
@@ -17,6 +25,11 @@ public class Controller {
     private MediaPlayer mediaPlayer;
     private String filePath;
 
+    @FXML
+    Slider volumeSlider;
+
+    @FXML
+    Slider timeSlider;
     @FXML
     private MediaView mediaView = new MediaView();
 
@@ -36,6 +49,28 @@ public class Controller {
             DoubleProperty height = mediaView.fitHeightProperty();
             width.bind(Bindings.selectDouble(mediaView.sceneProperty(),"width"));
             height.bind(Bindings.selectDouble(mediaView.sceneProperty(),"height"));
+
+            volumeSlider.setValue(mediaPlayer.getVolume() * 100);
+            volumeSlider.valueProperty().addListener(new InvalidationListener() {
+                @Override
+                public void invalidated(Observable observable) {
+                    mediaPlayer.setVolume(volumeSlider.getValue()/100);
+                }
+            });
+
+            mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+                @Override
+                public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+                    timeSlider.setValue(newValue.toSeconds());
+                }
+            });
+
+            timeSlider.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    mediaPlayer.seek(Duration.seconds(timeSlider.getValue()));
+                }
+            });
 
         }
     }
